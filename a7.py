02d5cf30e205bb6fe94f2324c323e1dc3885165f
Paul Kaczor <Pakaczor@cps.edu>
2023-04-24 09:30:03 -0500

    final

--- a7.py
+++ a7.py
@@ -1,63 +1,33 @@
 from neural import NeuralNet
+import os
+os.system('cls')
+xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 
-print("\n\nTraining SQ\n\n")
-sq_training_data = [
-    ([0.2], [0.04]),
-    ([0.3], [0.09]),
-    ([0.5], [0.25]),
-    ([0.7], [0.49]),
-    ([0.1], [0.01]),
-    ([.9], [.81])
-]
-sqn = NeuralNet(1, 20, 1)
-sqn.train(sq_training_data)
+xorn = NeuralNet(2, 10, 1)
+xorn.train(xor_training_data)
 
-print()
-print(sqn.test_with_expected(sq_training_data))
-print(sqn.evaluate([0.66]))
-print(sqn.evaluate([0.95]))
+print('\nTraining Voter Opinion\n')
 
-print()
-print("\n\nTraining xor\n\n")
-print()
+voter_opinion = [([0.9,0.6,0.8,0.3,0.1],[1.0]),
+    ([0.8,0.8,0.4,0.6,0.4],[1.0]),
+    ([0.7,0.2,0.4,0.6,0.3],[1.0]),
+    ([0.5,0.5,0.8,0.4,0.8],[0.0]),
+    ([0.3,0.1,0.6,0.8,0.8],[0.0]),
+    ([0.6,0.3,0.4,0.3,0.6],[0.0])]
 
-x_or_trainingdata = [
-    ([0, 0], [0]),
-    ([1, 0], [1]),
-    ([0, 1], [1]),
-    ([1, 1], [0])
-]
-
-xorn = NeuralNet(2, 20, 1)
-
-xorn.train(x_or_trainingdata)
-
-print()
-
-print(xorn.test_with_expected(x_or_trainingdata))
-
-print("\n\nTraining voter opinion\n\n")
-
-voter_opinion_data = [
-    ([.9, .6, .8, .3, .1], [1]),
-    ([.8, .8, .4, .6, .4], [1]),
-    ([.7, .2, .4, .6, .3], [1]),
-    ([.5, .5, .8, .4, .8], [0]),
-    ([.3, .1, .6, .8, .8], [0]),
-    ([.6, .3, .4, .3, .6], [0])
-]
-
-von = NeuralNet(5, 6, 1)
-
-von.train(voter_opinion_data)
-
-print(von.test_with_expected(voter_opinion_data))
+pie = NeuralNet(5,10,1)
+pie.train(voter_opinion)
 
 test_data = [
-    [1, 1, 1, .1, .1],
-    [.5, .2, .1, .7, .7]
+    [1,1,1,.1,.1],
+    [.5,.2,.1,.7,.7],
+    [.8,.3,.3,.3,.8],
+    [.8,.3,.3,.8,.3],
+    [.9,.8,.8,.3,.6]
 ]
 
-print(f"case 1: {test_data[0]} evaluates to: {von.evaluate(test_data[0])}")
-
-
+for x in range(5):
+    if float(von.evaluate(test_data[x])[0]) > 0.5:
+        print(f'case {x+1}: {test_data[x]} is 1')
+    if float(von.evaluate(test_data[x])[0]) < 0.5:
+        print(f'case {x+1}: {test_data[x]} is 0')
